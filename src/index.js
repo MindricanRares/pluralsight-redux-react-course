df898c72f1e506445ecd5fe60e18474f9c012f80
Rares <mindricanmihai@gmail.com>
2018-02-02 07:24:28 +0200

    - added manage course page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import { render } from "react-dom";
 import { Router, browserHistory } from "react-router";
 import routes from "./routes";
+import {loadCourses} from './actions/courseActions';
 import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap-theme.min.css";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,13 @@
-export default function courseReducer(state = [], action) {
-  switch (action.type) {
-    case "CREATE_COURSE":
-      return {
-        ...state,
-        course: action.course
-      };
+import * as types from '../actions/actionTypes';
 
+const initialState=[{
+  courses:["Test"]
+}];
+
+export default function courseReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.LOAD_COURSES_SUCCES:
+      return action.courses;
     default:
       return state;
   }
